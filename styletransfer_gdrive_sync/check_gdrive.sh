b35fc3e27f96f1a5244f1653542f8ca7666e0239
ixd-hof <michael.zoellner@hof-university.de>
2019-03-11 14:01:05 +0100

    Update check_gdrive.sh

--- styletransfer_gdrive_sync/check_gdrive.sh
+++ styletransfer_gdrive_sync/check_gdrive.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# workaround for CUDA problems
+#export PYTHONPATH=/usr/local/lib/python2.7/
+#export PATH=/usr/local/cuda-9.0/bin:$PATH
+#export LD_LIBRARY_PATH=/usr/local/cuda-9.0/lib64:$LD_LIBRARY_PATH
+#python --version
+#nvcc --version
+#id -un
+
 checkpoint_path="checkpoints_balloon"
 gdrive_path="/home/you/google-drive"
 transfer_path="$gdrive_path/Styletransfer/Transfer"
@@ -12,9 +20,10 @@ if [ -z "$(ls -A $transfer_path)" ]; then
 	echo "$transfer_path is empty"
 else
 	echo "images found..."
-    #cd /home/you/MachineLearning/fast-style-transfer
-    python evaluate.py --checkpoint $checkpoint_path --in-path $transfer_path --out-path $transfered_path --allow-different-dimensions
+	cd /home/you/MachineLearning/fast-style-transfer
+	python evaluate.py --checkpoint $checkpoint_path --in-path $transfer_path --out-path $transfered_path --allow-different-dimensions
     
-    echo "deleting source images..."
-    rm $transfer_path/*
+	echo "deleting source images..."
+	mkdir $transfer_path/_
+	mv $transfer_path/* $transfer_path/_
 fi
